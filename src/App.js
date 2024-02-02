ec1a5646c5a47afbd841358f2d5be609d60dd3b0
threalwinky <voquangvu09112006@gmail.com>
2024-02-02 08:47:38 +0700

    upload files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import Nav from'./components/Nav'
 
 function App() {
+  // const [count, setCount] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Nav />
+       <div className="scr">alhfkahlfk<br/>alhfkahlfk<br/>alhfkahlfk<br/>alhfkahlfk<br/></div>
+       <div className="scr2">alhfkahlfk<br/>alhfkahlfk<br/>alhfkahlfk<br/>alhfkahlfk<br/></div>
+       <div className="scr3">alhfkahlfk<br/>alhfkahlfk<br/>alhfkahlfk<br/>alhfkahlfk<br/></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
