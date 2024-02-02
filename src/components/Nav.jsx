e77dd3e683a63f0aab1591855b4b4e8460b510d7
threalwinky <voquangvu09112006@gmail.com>
2024-02-02 09:00:36 +0700

    upload files

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,12 +13,12 @@ const Nav = () => {
     ];
     let [open,setOpen]=useState(false);
   return (
-    <div className='shadow-md w-full fixed top-0 left-0'>
+    <div className=' w-full fixed top-22 left-0'>
       <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
       <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
       text-gray-800'>
         <span className='text-3xl text-indigo-600 mr-1 pt-2'>
-        <ion-icon name="logo-ionic"></ion-icon>
+        {/* <ion-icon name="logo-ionic"></ion-icon> */}
         </span>
       </div>
       
@@ -34,9 +34,9 @@ const Nav = () => {
             </li>
           ))
         }
-        <Button>
+        {/* <Button>
           Get Started
-        </Button>
+        </Button> */}
       </ul>
       </div>
     </div>
