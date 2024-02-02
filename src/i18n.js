d55136cbee8b3ee0ccc36762dd47042ba92061a7
threalwinky <voquangvu09112006@gmail.com>
2024-02-02 16:58:59 +0700

    upload files

---
+++ src/i18n.js
@@ -0,0 +1,45 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(LanguageDetector)
+  .init({
+    // we init with resources
+    resources: {
+      en: {
+        translations: {
+          "Hello": "Hello world.",
+          
+          "i18n": "This is I18n"
+        }
+      },
+      vn: {
+        translations: {
+          "Trang chủ": "Home",
+          "Giới thiệu": "Introduce",
+          "Mô hình toán học": "Mathmystic",
+          "Tài liệu học tập": "Document",
+          "Cửa hàng": "Products",
+          "Liên hệ": "Contact",
+        }
+      }
+    },
+    fallbackLng: 'en',
+
+    // have a common namespace used around the full app
+    ns: ['translations'],
+    defaultNS: 'translations',
+
+    keySeparator: false, // we use content as keys
+
+    interpolation: {
+      escapeValue: false, // not needed for react!!
+      formatSeparator: ','
+    },
+
+    react: {
+      wait: true
+    }
+  });
+
+export default i18n;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import './index.css';
-import App from './App';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import { I18nextProvider } from 'react-i18next';
+import { BrowserRouter } from 'react-router-dom';
+import i18n from './i18n';
+import App from './App';
+import "./App.css"
+import "./fontawesome.js"
+// append app to dom
 ReactDOM.render(
-  < App />,
+  <I18nextProvider i18n={ i18n }><BrowserRouter><App /></BrowserRouter>,</I18nextProvider>,
   document.getElementById('root')
 );
