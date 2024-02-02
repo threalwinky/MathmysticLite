b3a3578b0cabf5636b104447a3a0dd5af4aaa34b
threalwinky <voquangvu09112006@gmail.com>
2024-02-02 08:52:24 +0700

    upload files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Nav from'./components/Nav'
+import favicon from './components/img/favicon.jpg';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+        <div className="header">
+          <img src={favicon} className="rounded-full" />
+          <h1>MATHMYSTIC</h1>
+          {/* <img src="{require('./components/img/favicon.jpg')}"/> */}
+        </div>
        <Nav />
        <div className="scr">alhfkahlfk<br/>alhfkahlfk<br/>alhfkahlfk<br/>alhfkahlfk<br/></div>
        <div className="scr2">alhfkahlfk<br/>alhfkahlfk<br/>alhfkahlfk<br/>alhfkahlfk<br/></div>
