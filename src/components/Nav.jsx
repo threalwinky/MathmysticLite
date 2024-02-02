1d868d35a30f8115f5cb6c6194d4fce96411cd0b
threalwinky <voquangvu09112006@gmail.com>
2024-02-02 12:24:14 +0700

    upload files

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,20 +5,20 @@ import Button from './Button';
 const Nav = () => {
     let Links =[
       {name:"Trang chủ",link:"/"},
-      {name:"Giới thiệu",link:"/"},
-      {name:"Mô hình toán học",link:"/"},
-      {name:"Tài liệu học tập",link:"/"},
-      {name:"Cửa hàng",link:"/"},
-      {name:"Liên hệ", link:"/"}
+      {name:"Giới thiệu",link:"/intro"},
+      {name:"Mô hình toán học",link:"/about"},
+      {name:"Tài liệu học tập",link:"/doc"},
+      {name:"Cửa hàng",link:"/store"},
+      {name:"Liên hệ", link:"/contact"}
     ];
     let [open,setOpen]=useState(false);
   return (
     <div className=' w-full fixed top-22 left-0'>
-      <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+      <div className='md:flex items-center justify-between py-4 md:px-10 px-7 navbg'>
       <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
       text-gray-800'>
         <span className='text-3xl text-indigo-600 mr-1 pt-2'>
-        {/* <ion-icon name="logo-ionic"></ion-icon> */}
+        <ion-icon name="logo-ioni"></ion-icon>
         </span>
       </div>
       
@@ -26,7 +26,7 @@ const Nav = () => {
       <ion-icon name={open ? 'close':'menu'}></ion-icon>
       </div>
 
-      <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'} h nav`}>
+      <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'} nav`}>
         {
           Links.map((link)=>(
             <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
