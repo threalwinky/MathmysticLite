7df7707fa7a8c7223f9cd44a7b28608b0f962576
Vũ <voquangvu09112006@gmail.com>
2024-02-14 17:59:36 +0700

    upload files

--- src/routes/product/MainProduct.jsx
+++ src/routes/product/MainProduct.jsx
@@ -13,19 +13,57 @@ const MainProduct = ({ productInfo }) => {
 
   console.log(productInfo)
   const [zoom, setZoom] = useState(false)
+  // productInfo.imgUrl.push(productInfo.imgUrl[0])
+  // productInfo.imgUrl.push(productInfo.imgUrl[0])
   return (
     <div className='mmt__product' id='home'>
-      <div className='mmt__product-content'>
-        <Zoom>
-        <img src={productInfo.imgUrl}  />
-        </Zoom>
+
+      <div className='mmt__product-img'>
+        <img src={productInfo.imgUrl[0]} alt='' className='main' />
+        <div className='sub'>
+          {productInfo.imgUrl.map((img, index) => (
+            <div key={index}>
+              <img src={img} />
+            </div>
+          ))}
+        </div>
+
         
-        {productInfo.name}
-        {productInfo.price}
-        {productInfo.available}
       </div>
+      <div className='mmt__product-content'>
+          <div className='main'>
+            <span>Special</span>
+            <h1>Nike Invincible 3</h1>
+          </div>
+          <p className='intro'>Con un'ammortizzazione incredibile per sostenerti in tutti i tuoi chilometri, Invincible</p>
+          <h6 className='price'>$ 199.00</h6>
+          <div className='sub'>
+            <div className='content'>
+              <button>-</button>
+              <span>1</span>
+              <button>+</button>
+            </div>
+            <button>Add to Cart</button>
+          </div>
+        </div>
     </div>
   )
 }
 
 export default MainProduct
+
+{/* <Zoom>
+        <img src={productInfo.imgUrl[2]}  />
+        </Zoom>
+        {productInfo.name}
+        {productInfo.price}
+        {productInfo.available} */}
+{/* <h2>{productInfo.involve}</h2>
+        {productInfo.imgUrl.forEach((ea, index) => {
+         <img src={productInfo.imgUrl[2]}></img>  */}
+{/* // <h2 key={index}>{ea}</h2> */ }
+{/* {productInfo.imgUrl.map((img, index) => (
+          <div key={index}>
+            <img src={img} />
+          </div>
+        ))} */}
--- src/routes/product/Product.jsx
+++ src/routes/product/Product.jsx
@@ -4,8 +4,11 @@ import { useParams } from "react-router-dom";
 import { Header, Introduction, NavBar, Footer, About, Document, Store, Contact } from '../../components';
 import { PopUp, Loading, NotFound, Chatbot } from '../../containers'
 import MainProduct from './MainProduct'
-import Store1 from '../../assets/img/Store/Store1.webp'
-import Store2 from '../../assets/img/Store/Store2.webp'
+import Store1_1 from '../../assets/img/Store/Store1/Store1_1.webp'
+import Store1_2 from '../../assets/img/Store/Store1/Store1_2.webp'
+import Store1_3 from '../../assets/img/Store/Store1/Store1_3.png'
+import Store2_1 from '../../assets/img/Store/Store2/Store2_1.webp'
+import Store2_2 from '../../assets/img/Store/Store2/Store2_2.webp'
 const Product = () => {
 
     const params = useParams();
@@ -16,14 +19,20 @@ const Product = () => {
         {
             "id": "bo-dung-cu-hoc-tap-hinh-hoc-cho-hoc-sinh-khiem-thi",
             "name" : "Bo dung cu hoc tap hinh hoc cho hoc sinh khiem thi",
-            "imgUrl" : Store1,
+            "imgUrl" : [Store1_1, Store1_2, Store1_3],
             "price" : "0",
-            "available" : "0"
+            "available" : "0",
+            "involve" : `1 Bảng lỗ gỗ 20*20cm
+            2 cọng dây thun tròn
+            6 cọng dây thun dài 2 lỗ
+            4 cây ăng ten
+            3 Khoen tròn 
+            1 Flashcard hình học `
         },
         {
             "id": "bo-dung-cu-hinh-hoc",
             "name" : "Bo dung cu hinh hoc",
-            "imgUrl" : Store2,
+            "imgUrl" :  [Store2_1, Store2_2, Store1_3],
             "price" : "249000",
             "available" : "1"
         },
